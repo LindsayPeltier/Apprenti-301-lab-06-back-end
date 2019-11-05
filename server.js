d1650d1aad51ba927bf925638faebac2a720b2e5
Mark Swearingen <markswearingen@bendcable.com>
2019-11-05 11:01:56 -0800

    Remove express static

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
-app.use(express.static('./front-end'));
 // app.use(superAgent());  // Is this required?
 
 app.get('/', (request,response) => {
